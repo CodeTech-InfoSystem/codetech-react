9175655e6b5bf484e85edc93a9b18ae3c1f10b86
poojacodetech1 <pooja.rajput@codetechinfosystem.com>
2025-04-30 11:23:29 +0530

    add onsubmit on form

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -98,7 +98,7 @@ const ContactUs = () => {
         <div className="w-full lg:w-[40%] bg-white text-black p-6 rounded-3xl shadow-lg">
           <h3 className="text-xl font-semibold mb-4 text-center text-[#AC964F]">Get in Touch</h3>
 
-                    <form name="contact" method='post' data-netlify="true" netlify-honeypot="bot-field">
+                    <form name="contact" method='post' data-netlify="true" netlify-honeypot="bot-field" onSubmit={handleSubmit}>
                         <input type="hidden" name="form-name" value="contact" />
 
                         <label className="block text-sm font-medium text-gray-700 mb-1" htmlFor="name">
