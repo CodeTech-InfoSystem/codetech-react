fbf1d806edf1ee14cb6d36931f5c894383a13215
poojacodetech1 <pooja.rajput@codetechinfosystem.com>
2025-05-05 13:19:22 +0530

    chnages on Strengths.jsx

--- src/components/Strengths.jsx
+++ src/components/Strengths.jsx
@@ -104,10 +104,15 @@ const Strengths = () => {
                             <div className="h-1 w-4 bg-[#AD954F] rounded-lg"></div>
                         </div>
 
-                        <p className="text-[#000000] font-raleway">{strength.description}</p>
-                        <span className="absolute top-4 right-4 text-3xl font-bold text-gray-300">
-                            {`0${strength.id}`}
-                        </span>
+                        
+                        <ul className="text-[#000000] font-raleway list-disc pl-5">
+                                {strength.description.map((point, i) => (
+                                    <li key={i}>{point}</li>
+                                ))}
+                            </ul>
+                            <span className="absolute top-4 right-4 text-3xl font-bold text-gray-300">
+                                {`0${strength.id}`}
+                            </span>
                     </div>
                 ))}
             </div>
