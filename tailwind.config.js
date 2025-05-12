e6e029d12100490405024d53a5bd2697f2523f6b
poojacodetech1 <pooja.rajput@codetechinfosystem.com>
2025-05-12 12:11:19 +0530

    test container bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,19 @@ export default {
       },
     },
   },
+  container: {
+    center: true,
+    padding: '1rem',
+    screens: {
+      sm: "600px",
+      md: "728px",
+      lg: "984px",
+      xl: "1240px",
+      "2xl": "1496px"
+    },
+  },
   plugins: [
     require("tailwind-scrollbar-hide"),
   ],
 };
+
